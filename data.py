5361bc800853c956257d369e9586642390ed73a5
mahdikhoshdel <mohsenkhoshdl@gmail.com>
2023-07-03 17:20:13 +0330

    Clean up code.

--- data.py
+++ data.py
@@ -1,17 +1,3 @@
-# questions_data = [
-#     {"text": "A slug's blood is green.", "answer": "True"},
-#     {"text": "The loudest animal is african elephant.", "answer": "False"},
-#     {"text": "Blue is red", "answer": "False"},
-#     {"text": "2 ** 3 = 8", "answer": "True"},
-#     {"text": "oop is just for python programming language", "answer": "False"},
-#     {"text": "Smoking is very dangerous", "answer": "True"},
-#     {"text": "Smoking is a healthy way to controlling stress", "answer": "False"},
-#     {"text": "You are in planet earth", "answer": "True"},
-#     {"text": "You are a alien", "answer": "False"},
-#     {"text": "You live on moon", "answer": "False"},
-#     {"text": "Earth is dying.", "answer": "True"},
-#     {"text": "Sky is Blue", "answer": "True"}
-# ]
 import requests
 
 params = {
@@ -22,6 +8,3 @@ response = requests.get('https://opentdb.com/api.php', params=params)
 response.raise_for_status()
 data = response.json()
 questions_data = data['results']
-print(questions_data)
-# correct_answer
-# question
