32c0458c4a292de9bb9ba9d03846864b7e1cde4c
mahdikhoshdel <mohsenkhoshdl@gmail.com>
2023-07-03 17:16:41 +0330

    Changed Questions method from manually to API call

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from quiz_brain import QuizBrain
 
 questions_bank = []
 for question in questions_data:
-    question_text = question['text']
-    question_answer = question['answer']
+    question_text = question['question']
+    question_answer = question['correct_answer']
     new_question = Question(question_text, question_answer)
     questions_bank.append(new_question)
 
