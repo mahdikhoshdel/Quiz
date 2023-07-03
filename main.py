77b56c2b2d01751524469616bb3345ee65d5e4f2
mahdikhoshdel <mohsenkhoshdl@gmail.com>
2023-07-03 18:38:09 +0330

    -Added UI using Tkinter -Adjusted functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from data import questions_data
 from question_model import Question
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 questions_bank = []
 for question in questions_data:
@@ -10,9 +11,9 @@ for question in questions_data:
     questions_bank.append(new_question)
 
 quiz = QuizBrain(questions_bank)
-
-while quiz.still_has_questions():
-    quiz.next_question()
+quiz_ui = QuizInterface(quiz)
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz.")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,24 +1,27 @@
+import html
+
+
 class QuizBrain:
 
     def __init__(self, question_list):
         self.question_list = question_list
         self.question_number = 0
         self.score = 0
+        self.current_question = None
 
     def still_has_questions(self):
         return len(self.question_list) > self.question_number
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        answer = input(f'Q.{self.question_number}: {current_question.text}. (True/False): ')
-        self.check_answer(answer, current_question.answer)
+        unescape_question_text = html.unescape(self.current_question.text)
+        return f'Q.{self.question_number}\n\n{unescape_question_text}.'
 
-    def check_answer(self, user_answer, correct_answer):
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
-            print("\nCorrect!\n")
             self.score += 1
+            return True
         else:
-            print("\nWrong answer!")
-        print(f"The correct answer was: {correct_answer}")
-        print(f"Your current score is: {self.score}/{self.question_number}\n")
+            return False
